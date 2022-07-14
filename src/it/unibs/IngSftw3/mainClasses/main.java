f2cde39df69c80ddf68a8a6f61095bed1c012174
Zambo17 <e.zambelli008@studenti.unibs.it>
2022-07-14 20:20:36 +0200

    UML classi

--- src/it/unibs/IngSftw3/mainClasses/main.java
+++ src/it/unibs/IngSftw3/mainClasses/main.java
@@ -76,7 +76,9 @@ public class main {
         System.out.println("\nFINE PROGRAMMA");
         if(offerte.getListaOfferte().size()!=0)
             XmlWriter.salvaOfferte(offerte, "offerte.xml");
-        XmlWriter.salvaParametri(conf.getParametri(),"parametriSalvati.xml");
+        if(param != null) {
+            XmlWriter.salvaParametri(conf.getParametri(), "parametriSalvati.xml");
+        }
         if(conf.getSis().getListaGerarchie().size()!=0){
             XmlWriter.salvaSistema(conf.getSis(), "sistema.xml");
         }
